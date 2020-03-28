4cbf80fafcafea63ecc27d37d351573ab3622452
hoijui <hoijui.quaero@gmail.com>
2020-03-28 15:20:28 +0100

    parse_3dd.py: cleanup [nlc]

--- frame3dd/parse_3dd.py
+++ frame3dd/parse_3dd.py
@@ -8,19 +8,18 @@ def parse_line(line):
 	for data in line.split():
 		try:
 			res.append(float(data))
-		except:
+		except ValueError:
 			res.append(data)
 	return res
 
 class Table:
-	def __init__(self,title,names,data):
-		self.title = title.replace("\t\t","\t").replace("\t"," ")
+	def __init__(self, title, names, data):
+		self.title = title.replace("\t\t", "\t").replace("\t", " ")
 		self.names = names.split()
 		self.data = data
 
-	def max(self,column):
-		return max( [ v[column] for v in self.data ] )
-
+	def max(self, column):
+		return max([v[column] for v in self.data])
 
 def parse(filename):
 	mode = "scan"
@@ -36,7 +35,7 @@ def parse(filename):
 			if mode == "scan":
 				mode = "dump"
 				table_data = []
-				table = Table( lines[-3], lines[-2], table_data)
+				table = Table(lines[-3], lines[-2], table_data)
 				tables.append(table)
 			if mode == "dump":
 				table_data.append(parse_line(line))
@@ -47,20 +46,12 @@ def parse(filename):
 
 	return tables
 
-
 if __name__ == '__main__':
-
-
-	import json
+	#import json
 	tables = parse("traverse.out")
 
-
-	for i,table in enumerate(tables):
-		print i,table.title
-		# print tables[.max(3)
-
+	for i, table in enumerate(tables):
+		print(i, table.title)
+		#print(tables[.max(3))
 
 	#print json.dumps(tables, indent=4)
-
-
-
